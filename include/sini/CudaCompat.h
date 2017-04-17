0b01547e2d187da8304fadd5340aa3f8b7e0b64b
Simon Nilsson <sim.f.nilsson@gmail.com>
2017-04-17 12:13:07 +0200

    Added CudaCompat since the latest Matrix and Vector code needs it.

---
+++ include/sini/CudaCompat.h
@@ -0,0 +1,19 @@
+// Compatibility with CUDA to allow some functions to be called both from the
+// host and the device, if nvcc is used to compile.
+#pragma once
+#ifndef SINI_CUDA_COMPAT_H
+#define SINI_CUDA_COMPAT_H
+
+#ifdef __CUDACC__
+#define SINI_CUDA_COMPAT inline __host__ __device__
+#define SINI_CUDA_KERNEL __global__
+#else
+#define SINI_CUDA_COMPAT inline
+#endif
+
+// Allows for device/host specific CUDA code
+#if defined(__CUDACC__) && defined(__CUDA_ARCH__)
+#define SINI_CUDA_DEVICE
+#endif
+
+#endif
